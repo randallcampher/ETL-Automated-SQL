29ae52e396ddc3e5dd667bbb3f5aff52b9f83cf8
Randall Campher <randycampher@gmail.com>
2025-04-11 01:00:03 +0200

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -119,12 +119,12 @@ BEGIN
 			sls_ord_num,
 			sls_prd_key,
 			sls_cust_id,
-			CASE WHEN LEN(sls_order_dt) != 8 OR sls_order_dt <= 0 THEN NULL ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE) END,
-			CASE WHEN LEN(sls_ship_dt) != 8 OR sls_ship_dt <= 0 THEN NULL ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE) END,
-			CASE WHEN LEN(sls_due_dt) != 8 OR sls_due_dt <= 0 THEN NULL ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE) END,
-			CASE WHEN sls_sales != sls_quantity * ABS(sls_price) OR sls_sales IS NULL THEN sls_quantity * ABS(sls_price) ELSE sls_sales END,
+			CASE WHEN LEN(sls_order_dt) != 8 OR sls_order_dt <= 0 THEN NULL ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE) END AS sls_order_dt,
+			CASE WHEN LEN(sls_ship_dt) != 8 OR sls_ship_dt <= 0 THEN NULL ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE) END AS sls_ship_dt,
+			CASE WHEN LEN(sls_due_dt) != 8 OR sls_due_dt <= 0 THEN NULL ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE) END AS sls_due_dt,
+			CASE WHEN sls_sales != sls_quantity * ABS(sls_price) OR sls_sales IS NULL THEN sls_quantity * ABS(sls_price) ELSE sls_sales END AS sls_sales,
 			sls_quantity,
-			CASE WHEN sls_price IS NULL OR sls_price <= 0 THEN sls_sales / NULLIF(sls_quantity, 0) ELSE sls_price END
+			CASE WHEN sls_price IS NULL OR sls_price <= 0 THEN sls_sales / NULLIF(sls_quantity, 0) ELSE sls_price END AS sls_price
 		FROM bronze.crm_sales_details;
 		SET @end_time = GETDATE();
 		PRINT '>>>>>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' sec';
